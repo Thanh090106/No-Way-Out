472dc240ffe8ce51438fc214d93a798a0fdedb7a
Carson09092006 <144376294+Carson09092006@users.noreply.github.com>
2024-05-05 21:34:23 -0500

    added walking sounds

---
+++ Assets/Footsteps.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footsteps : MonoBehaviour
+{
+    public AudioSource footstepsSound, sprintSound;
+    
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.S))
+        {
+            footstepsSound.enabled = true; 
+        }
+        else
+        {
+            footstepsSound.enabled = false; 
+        }
+    }
+
+    
+    
+}
